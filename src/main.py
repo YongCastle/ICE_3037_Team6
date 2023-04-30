4e9a1caf45f1b8f3efe2d08d7fd2c3f18bddac49
YongCastle <xidid430rr@gmail.com>
2023-04-30 14:27:39 +0900

    update

--- src/main.py
+++ src/main.py
@@ -4,3 +4,15 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 
+import serial
+
+ser = serial.Serial('/dev/ttyACM0', 9600) # 아두이노와 연결된 포트 번호와 시리얼 통신 속도를 설정합니다.
+
+while True:
+    try:
+        data = ser.readline().decode('utf-8').rstrip()
+        print(data)
+    except KeyboardInterrupt:
+        break
+
+ser.close()6
